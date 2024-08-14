52fa93eb9ffa94b947804240deadb76e62fc58bd
jsnam <skawotkd12@naver.com>
2024-08-14 13:50:14 +0900

    jsnam 블로그 끝

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,35 @@
 <template>
-  <div class="black-bg" v-if="모달창열렸니 == true">
-    <div class="white-bg">
-      <h4>{{ 원룸들[누른거].title }}</h4>
-      <p>{{원룸들[누른거].content}}</p>
-      <button @click="모달창열렸니 = false">닫기</button>
-    </div>
-  </div>
-  
+  <transition name="fade">
+    <Modal @CloseModal="모달창열렸니 = false" :원룸들=원룸들 :누른거=누른거 :모달창열렸니=모달창열렸니></Modal>
+  </transition>
+
   <div class="menu">
     <a v-for="(메뉴들, idx) in 메뉴들" :key="idx"> {{ 메뉴들 }} </a>
   </div>
 
-  <div v-for="(data,idx) in 원룸들" :key="idx">
-    <img :src="data.image" class="room-img">
-    <h4 @click="모달창열렸니 = true; 누른거= idx">{{ data.title }}</h4>
-    <p>{{data.price}} 만원</p>
-  </div>
+  <Discount v-if="showDiscount == true" :count = count></Discount>
+  <button @click="priceSort">가격순정렬</button>
+  <button @click="sortBack">되돌리기</button>
+
+  <Card @openModal="모달창열렸니 = true" :원룸들=원룸들></Card>
 </template>
 
 <script>
 import data from './assets/oneroom';
+import Discount from './Discount.vue';
+import Modal from './Modal.vue';
+import Card from './Card.vue';
 
 export default {
   name: 'App',
   data() {
     return {
-      누른거 : 0,
-      원룸들 : data,
-      모달창열렸니 : false,
+      count : 30,
+      showDiscount: true,
+      원룸들오리지널: [...data],
+      누른거: 0,
+      원룸들: data,
+      모달창열렸니: false,
       신고수: [0, 0, 0],
       메뉴들: ['Home', "Shop", "About"],
       produsts: ['역삼동원룸', '천호동원룸', '마포구원룸']
@@ -35,33 +37,69 @@ export default {
   },
 
   methods: {
+    priceSort() {
+      this.원룸들.sort(function (a, b) {
+        return a.price - b.price;
+      })
+    },
+
+    sortBack() {
+      this.원룸들 = [...this.원룸들오리지널];
+    }
+  },
+
+  mounted() {
+    // setTimeout(() => {
+    //   this.showDiscount = false;
+    // }, 2000)
+
+    setInterval(()=> {
+      if(this.count > 0){
+        this.count--;
+      }
+    },1000)
+
 
   },
 
-
   components: {
+    Discount: Discount,
+    Modal: Modal,
+    Card: Card
   }
 }
 </script>
 
 <style>
+.discount {
+  background: #eee;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 5px;
+}
 
 body {
-  margin : 0;
+  margin: 0;
 }
+
 div {
   box-sizing: border-box;
 }
+
 .black-bg {
-  width: 100%; height:100%;
-  background: rgba(0,0,0,0.5);
-  position: fixed; padding: 20px;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
 }
+
 .white-bg {
-  width: 100%; background: white;
+  width: 100%;
+  background: white;
   border-radius: 8px;
   padding: 20px;
-} 
+}
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -69,7 +107,7 @@ div {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 
 .menu {
---
+++ src/Discount.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="discount">
+        <h4>지금 결제하면 {{count}}% 할인</h4>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'DiscountVue',
+    props: {
+        count : Number
+    }
+}
+</script>
+
+<style></style>
