462d6f4cd17a751428f6776bc7c2045ff129e8dd
jsnam <skawotkd12@naver.com>
2024-08-15 16:20:46 +0900

    jsnam

---
+++ src/components/Container.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Post :postData = "postData"/>
+  </div>
+</template>
+
+<script>
+import Post from '@/components/Post.vue'
+export default {
+    name:'containerVue',
+    components : {
+      Post : Post
+    },
+    props : {
+      postData : Array
+    }
+}
+</script>
+
+<style>
+
+</style>
